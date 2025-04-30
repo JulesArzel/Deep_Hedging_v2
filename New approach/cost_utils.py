a189b14c09ea1c3f20a2fe71c2ea1aa659e44589
Jules Arzel <julesarzel@gmail.com>
2025-04-30 18:14:00 +0200

    start callspread

--- New approach/cost_utils.py
+++ New approach/cost_utils.py
@@ -51,3 +51,23 @@ def buyer(x, y, cost_b, cost_s, K):
                 cost[i][j] = ((1 + cost_b) * (y[j] + 1) * np.exp(x[i])) - K
 
     return cost
+
+
+def callspread(x, y, cost_b, cost_s, K1, K2):
+    cost = np.zeros((len(x), len(y)))
+
+    for i in range(len(x)):
+        for j in range(len(y)):
+            if y[j] < 0 and np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2) <= 0:
+                cost[i][j] = (1 + cost_b) * y[j] * np.exp(x[i])
+
+            elif y[j] >= 0 and np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2) <= 0:
+                cost[i][j] = (1 - cost_s) * y[j] * np.exp(x[i])
+
+            elif y[j] >= 0 and np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2) > 0:
+                cost[i][j] = ((1 - cost_s) * (y[j]) * np.exp(x[i])) + np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2)
+
+            elif y[j] < 0 and np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2) > 0:
+                cost[i][j] = ((1 + cost_b) * (y[j]) * np.exp(x[i])) + np.max(np.exp(x[i])-K1,0)-np.max(np.exp(x[i])-K2)
+
+    return cost
